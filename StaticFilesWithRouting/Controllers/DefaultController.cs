90a51ede26bde3559e7da6d9f16336f1aa220c4b
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-04-29 12:36:45 +0530

    The bug

---
+++ StaticFilesWithRouting/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaticFilesWithRouting.Controllers
+{
+    public class DefaultController: Controller
+    {
+
+        // commenting following will make static files work
+        [HttpGet("{*all}", Order = int.MaxValue)]
+        public IActionResult DefaultRoute()
+        {
+            return Content("Its me always !!! Static files are not working");
+        }
+
+    }
+
+
+}
